1dcad8d3c69db9af6a61c80aa2562525d5e76929
Umar Farooq <ufservices465@gmail.com>
2024-07-18 22:18:13 +0530

    create Day_4.js

---
+++ Day_4.js
@@ -0,0 +1,20 @@
+// Day 4: Loops
+
+// Acitivity 1:
+
+// Task 1: write a program to print the numbers from 1 to 100 using a for loop.
+
+let i = 1
+
+for (i = 1; i <=100; i++) {
+    console.log(i)
+}
+
+// Task 2: write a program to print the multiplication table of 5 using a for loop
+
+let tableFive = 5;
+
+for (let i = 1; i <= 10; i++) {
+   console.log(`5 X ${i} is ${tableFive * i}`)
+}                                                                                                         
+      
